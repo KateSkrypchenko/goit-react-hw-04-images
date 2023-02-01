565213b822ed3b481e2964061b8ee48412521ac8
user <katalonij440@gmail.com>
2023-02-01 22:54:16 +0200

    libraries

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,55 +1,90 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import { Item, Image } from './ImageGalleryItem.styled';
 
 import { Modal } from 'components/Modal/Modal';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
+export const ImageGalleryItem = ({ largeImageURL, webformatURL, tags }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
   };
 
-  openModal = () => {
-    this.setState({
-      showModal: true,
-    });
-  };
-
-  handleKeyDown = event => {
+  const handleKeyDown = event => {
     if (event.code === 'Escape') {
-      this.setState({
-        showModal: false,
-      });
+      setShowModal(false);
     }
   };
 
-  handleBackdropClick = event => {
+  const handleBackdropClick = event => {
     if (event.target === event.currentTarget) {
-      this.setState({
-        showModal: false,
-      });
+      setShowModal(false);
     }
   };
 
-  render() {
-    const { showModal } = this.state;
-    const { largeImageURL, webformatURL, tags } = this.props;
-    return (
-      <div>
-        <Item>
-          <Image src={webformatURL} alt={tags} loading="lazy" onClick={this.openModal} />
-        </Item>
-        {showModal && (
-          <Modal
-            largeImageURL={largeImageURL}
-            onClose={this.handleBackdropClick}
-            onChange={this.handleKeyDown}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Item>
+        <Image src={webformatURL} alt={tags} loading="lazy" onClick={openModal} />
+      </Item>
+      {showModal && (
+        <Modal
+          largeImageURL={largeImageURL}
+          onClose={handleBackdropClick}
+          onChange={handleKeyDown}
+        />
+      )}
+    </div>
+  );
+};
+
+// export class ImageGalleryItem extends Component {
+//   state = {
+//     showModal: false,
+//   };
+
+//   openModal = () => {
+//     this.setState({
+//       showModal: true,
+//     });
+//   };
+
+//   handleKeyDown = event => {
+//     if (event.code === 'Escape') {
+//       this.setState({
+//         showModal: false,
+//       });
+//     }
+//   };
+
+//   handleBackdropClick = event => {
+//     if (event.target === event.currentTarget) {
+//       this.setState({
+//         showModal: false,
+//       });
+//     }
+//   };
+
+//   render() {
+//     const { showModal } = this.state;
+//     const { largeImageURL, webformatURL, tags } = this.props;
+//     return (
+//       <div>
+//         <Item>
+//           <Image src={webformatURL} alt={tags} loading="lazy" onClick={this.openModal} />
+//         </Item>
+//         {showModal && (
+//           <Modal
+//             largeImageURL={largeImageURL}
+//             onClose={this.handleBackdropClick}
+//             onChange={this.handleKeyDown}
+//           />
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
 ImageGalleryItem.propTypes = {
   largeImageURL: PropTypes.string.isRequired,
